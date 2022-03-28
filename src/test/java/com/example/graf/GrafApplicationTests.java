0e4e4049f9f5fe48a5e2f3db75c6f96e5552fbaa
lehaTumbaev <leha.tumbaev@gmail.com>
2022-03-28 16:32:23 +0700

    initial commit

---
+++ src/test/java/com/example/graf/GrafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
